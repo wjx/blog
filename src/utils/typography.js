2c352c20fcafab3a37e2d57542abf9b44ee65410
Jiaxing Wang <hello.wjx@gmail.com>
2021-07-10 12:17:05 +0800

    Update typography theme

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,5 +1,8 @@
 import Typography from "typography"
 import Wordpress2016 from "typography-theme-wordpress-2016"
+import funstonTheme from 'typography-theme-funston'
+import wikipediaTheme from 'typography-theme-Wikipedia'
+import stowLakeTheme from 'typography-theme-stow-lake'
 
 Wordpress2016.overrideThemeStyles = () => {
   return {
@@ -11,7 +14,7 @@ Wordpress2016.overrideThemeStyles = () => {
 
 delete Wordpress2016.googleFonts
 
-const typography = new Typography(Wordpress2016)
+const typography = new Typography(stowLakeTheme)
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
